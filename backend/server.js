b7ebb821fd1006ca2ae5d17ad7aa057ca877f5e8
Adhigv <adarshagv7@gmail.com>
2026-06-15 20:08:06 +0000

    Updated cors url api-gyks

--- backend/server.js
+++ backend/server.js
@@ -14,16 +14,17 @@ connectDB();
 const app = express();
 
 app.use(
-  cors(
-    // origin:[
+  cors({
+    origin:[
       
     //   "https://gyks.vercel.app",
     //   "https://gyks-q0yiswzw4-adarsha-g-v-s-projects.vercel.app",
-    //   "https://gyks.in",
-    //   "https://www.gyks.in"
-    // ],
-    // credentials:true,
-  )
+      "https://gyks.in",
+      "https://www.gyks.in",
+      "https://api.gyks.in"
+    ],
+    credentials:true,
+  })
 );
 
 app.use(express.json());
